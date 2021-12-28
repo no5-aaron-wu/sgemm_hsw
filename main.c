198b769589f4e6a4f8b852905c279830039d4b3c
aaron-wsl-ubuntu <wuhao@insta360.com>
2021-12-28 15:13:06 +0800

    add 4x2 micro-kernel test

--- main.c
+++ main.c
@@ -19,6 +19,12 @@ void sgemm_kernel_x64_fma_m4n24(float *a,
     int m,
     int k);
 
+void sgemm_kernel_x64_fma_m4n16(float *a,
+    float *b,
+    float *c,
+    int m,
+    int k);
+
 #ifdef __cplusplus
 }
 #endif
@@ -83,15 +89,21 @@ int main(int argc, char *argv[])
 {
     int i;
 
-    if (argc != 3)
+    if (argc != 4)
     {
-        fprintf(stderr, "Usage: %s m k\n", argv[0]);
-        return 0;
+        fprintf(stderr, "Usage: %s m k 24/16\n", argv[0]);
+        return -1;
     }
 
     int m = atoi(argv[1]);
     int k = atoi(argv[2]);
-    long comp = 2L * m * k * 24L;
+    int n = atoi(argv[3]);
+    if (n != 24 && n != 16){
+        fprintf(stderr, "n must be 24 or 16 currently\n");
+        return -1;
+    }
+
+    long comp = 2L * m * k * n;
     int loop_time = (int)(2e11 / comp);
 
     struct timespec start, end;
@@ -100,9 +112,9 @@ int main(int argc, char *argv[])
     thread_bind(0);
 
     float *a = (float*)page_alloc(m * k * sizeof(float));
-    float *b = (float*)page_alloc(k * 24 * sizeof(float));
-    float *c1 = (float*)page_alloc(m * 24 * sizeof(float));
-    float *c2 = (float*)page_alloc(m * 24 * sizeof(float));
+    float *b = (float*)page_alloc(k * n * sizeof(float));
+    float *c1 = (float*)page_alloc(m * n * sizeof(float));
+    float *c2 = (float*)page_alloc(m * n * sizeof(float));
 
     srand(time(NULL));
 
@@ -110,44 +122,65 @@ int main(int argc, char *argv[])
     {
         a[i] = (float)rand() / (float)RAND_MAX;
     }
-    for (i = 0; i < k * 24; i++)
+    for (i = 0; i < k * n; i++)
     {
         b[i] = (float)rand() / (float)RAND_MAX;
     }
 
-    // fma-tuned version
-    // warm up
-    for (i = 0; i < loop_time; i++)
-    {
-        sgemm_kernel_x64_fma_m4n24(a, b, c2, m, k);
+    if (n == 24){
+        // fma-tuned version
+        // warm up
+        for (i = 0; i < loop_time; i++)
+        {
+            sgemm_kernel_x64_fma_m4n24(a, b, c2, m, k);
+        }
+        clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+        for (i = 0; i < loop_time; i++)
+        {
+            sgemm_kernel_x64_fma_m4n24(a, b, c2, m, k);
+        }
+        clock_gettime(CLOCK_MONOTONIC_RAW, &end);    
+    } else if (n == 16){
+        // fma-tuned version
+        // warm up
+        for (i = 0; i < loop_time; i++)
+        {
+            sgemm_kernel_x64_fma_m4n16(a, b, c2, m, k);
+        }
+        clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+        for (i = 0; i < loop_time; i++)
+        {
+            sgemm_kernel_x64_fma_m4n16(a, b, c2, m, k);
+        }
+        clock_gettime(CLOCK_MONOTONIC_RAW, &end);    
     }
-    clock_gettime(CLOCK_MONOTONIC_RAW, &start);
-    for (i = 0; i < loop_time; i++)
-    {
-        sgemm_kernel_x64_fma_m4n24(a, b, c2, m, k);
-    }
-    clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+    
 
     t = get_time(&start, &end) / loop_time;
     gflops = (double)comp / t * 1e-9;
 
     printf("sgemm_kernel_x64_fma(%d, %d, %d): time = %lf us, perf = %lf GFLOPS.\n", m, 24, k, t * 1e6, gflops);
 
-    memset(c1, 0, m * 24 * sizeof(float));
-    memset(c2, 0, m * 24 * sizeof(float));
-    sgemm_naive(a, b, c1, m, 24, k);
-    sgemm_kernel_x64_fma_m4n24(a, b, c2, m, k);
-    save_bin("naive.bin", c1, m * 24);
-    save_bin("tuned.bin", c2, m * 24);
+    memset(c1, 0, m * n * sizeof(float));
+    memset(c2, 0, m * n * sizeof(float));
+    sgemm_naive(a, b, c1, m, n, k);
+    if (n == 24){
+        sgemm_kernel_x64_fma_m4n24(a, b, c2, m, k);
+    } else if (n == 16){
+        sgemm_kernel_x64_fma_m4n16(a, b, c2, m, k);
+    }
+    
+    save_bin("naive.bin", c1, m * n);
+    save_bin("tuned.bin", c2, m * n);
 
     printf("sgemm_naive result: naive.bin\n");
     printf("sgemm_kernel_x64_fma_m4n24 result: tuned.bin\n");
     printf("Use fp_diff(https://github.com/pigirons/fp_diff) to compare the results.\n");
 
     page_free(a, m * k * sizeof(float));
-    page_free(b, k * 24 * sizeof(float));
-    page_free(c1, m * 24 * sizeof(float));
-    page_free(c2, m * 24 * sizeof(float));
+    page_free(b, k * n * sizeof(float));
+    page_free(c1, m * n * sizeof(float));
+    page_free(c2, m * n * sizeof(float));
     
     return 0;
 }
